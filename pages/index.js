71dd8e121e66dee29ddd1caf3b27996b3d6ee79a
DeskyWesky <givonnisardina12@gmail.com>
2026-07-24 20:08:18 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,17 @@
+export default function Home() {
+  return (
+    <div className="card">
+      <img src="/avatar.png" className="avatar" alt="avatar" />
+      <h1 className="username">yourname</h1>
+      <p className="bio">your bio text</p>
 
+      <div className="links">
+        <a href="https://discord.com/users/YOURID" target="_blank">Discord</a>
+        <a href="https://instagram.com/yourhandle" target="_blank">Instagram</a>
+        <a href="https://cash.app/$yourcashtag" target="_blank">CashApp</a>
+      </div>
+
+      <a href="/signup" className="make-own-btn">Click To Make Your Own</a>
+    </div>
+  );
+}
