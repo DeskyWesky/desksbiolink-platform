164b063ec6d32b84140ffecb2a54a242f8e708a8
DeskyWesky <givonnisardina12@gmail.com>
2026-07-24 20:33:04 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import ParticleBackground from '../components/ParticleBackground';
 
 export default function Home() {
   const bioText = "your bio text";
@@ -15,18 +16,19 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="card">
-      <img src="/avatar.png" className="avatar" alt="avatar" />
-      <h1 className="username">yourname</h1>
-      <p className="bio">{displayedBio}</p>
-
-      <div className="links">
-        <a href="https://discord.com/users/YOURID" target="_blank">Discord</a>
-        <a href="https://instagram.com/yourhandle" target="_blank">Instagram</a>
-        <a href="https://cash.app/$yourcashtag" target="_blank">CashApp</a>
+    <>
+      <ParticleBackground />
+      <div className="card">
+        <img src="/avatar.png" className="avatar" alt="avatar" />
+        <h1 className="username">yourname</h1>
+        <p className="bio">{displayedBio}</p>
+        <div className="links">
+          <a href="https://discord.com/users/YOURID" target="_blank">Discord</a>
+          <a href="https://instagram.com/yourhandle" target="_blank">Instagram</a>
+          <a href="https://cash.app/$yourcashtag" target="_blank">CashApp</a>
+        </div>
+        <a href="/signup" className="make-own-btn">Click To Make Your Own</a>
       </div>
-
-      <a href="/signup" className="make-own-btn">Click To Make Your Own</a>
-    </div>
+    </>
   );
 }
